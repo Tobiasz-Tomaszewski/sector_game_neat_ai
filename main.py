f35c53d23a5a26c98223c3013af56da850e27c87
Tobiasz Tomaszewski <tpt484@poczta.onet.pl>
2024-01-20 17:11:18 +0100

    Added main.py to tracking.

---
+++ main.py
@@ -0,0 +1,42 @@
+import settings
+from ScreenClasses import *
+from GameLogicClassesAndHandlers import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((settings.width, settings.height))
+
+clock = pygame.time.Clock()
+running = True
+music_play = True
+dt = 0
+player = Player(centre, 100, 15, curve_nr=0, path_deviation=0, player_speed=400)
+obstacle_handler = ObstacleHandler(45, 270, 200)
+game = Game(player, obstacle_handler)
+text_handler = TextHandler(40)
+difficulty_handler = DifficultyHandler()
+
+#neat_config_file = "config.txt"
+#config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation,
+#                     neat_config_file)
+
+while running:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+
+    game.handle_screen(text_handler, screen, dt)
+    game.handle_events(dt, events)
+    game.detect_collision()
+
+    mouse = pygame.mouse.get_pos()
+
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-independent physics.
+    dt = clock.tick(60) / 1000
+
+
+pygame.quit()
